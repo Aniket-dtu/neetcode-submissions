71cbfed3b54b1a22c0276cbe3d4610282a504cf8
Aniket-dtu <satyapal643@gmail.com>
2026-07-20 03:26:03 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int z) {
+        double x = z;
+        double y = 1;
+        double e = 0.000001;   // baby lonion methord
+
+        while (abs(x - y) > e) {
+         x = (x + y) / 2;
+         y = z / x;
+        }
+        return  x;
+        
+    }
+};
